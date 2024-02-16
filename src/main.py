0defd2463a6ff205b550ec7c11f75898c118bb38
zlElo <mail@zlelo.de>
2024-02-16 10:08:48 +0100

    no retrys password cracker

--- src/main.py
+++ src/main.py
@@ -70,13 +70,18 @@ def check():
 
         print("Total count: "+str(count),end=' ')
 
+        used_passwords = set()
         for i in range(count):
-            if generated != password:
-                count2 += 1
+            while True:
                 generated = ''.join(secrets.choice(uses) for i in range(int(length)))
-                sys.stdout.flush()
-                print("\rSteps try crack: " + str(i+1) + "/" + str(count) + '   ' + 'generated password: ' + generated, end=' ')
-            else:
+                if generated not in used_passwords: 
+                    used_passwords.add(generated)
+                    break
+            
+            count2 += 1
+            sys.stdout.flush()
+            print("\rSteps try crack: " + str(i+1) + "/" + str(count) + ' ' + 'generated password: ' + generated, end=' ')
+            if generated == password:
                 break
 
         print("")#to change the current line
